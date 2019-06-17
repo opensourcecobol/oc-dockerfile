e360d812b22029c76d5d7f5ec43b85e67ea02a4e
n-isaka <n-isaka@tsh-world.co.jp>
2019-06-17 14:05:45 +0900

    (add) Create Dockerfile and samples
    This repository is Moved from my own repository.

---
+++ HELLO.cbl
@@ -0,0 +1,37 @@
+      ******************************************************************
+      * Copyright 2019, Tokyo System House Co., Ltd. <opencobol@tsh-world.co.jp>
+      *
+      * Licensed under the Apache License, Version 2.0 (the "License");
+      * you may not use this file except in compliance with the License.
+      * You may obtain a copy of the License at
+      *
+      *     http://www.apache.org/licenses/LICENSE-2.0
+      *
+      * Unless required by applicable law or agreed to in writing, software
+      * distributed under the License is distributed on an "AS IS" BASIS,
+      * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      * See the License for the specific language governing permissions and
+      * limitations under the License.
+      *
+      ******************************************************************
+      *  opencobol SAMPLE
+      *
+      *  Copyright 2019 Tokyo System House Co., Ltd.
+      ******************************************************************
+       IDENTIFICATION              DIVISION.
+      ******************************************************************
+       PROGRAM-ID.                 HELLO.
+       AUTHOR.                     TSH.
+       DATE-WRITTEN.               2019-04-19.
+
+      ******************************************************************
+       DATA                        DIVISION.
+      ******************************************************************
+       WORKING-STORAGE             SECTION.
+      ******************************************************************
+       PROCEDURE                   DIVISION.
+      ******************************************************************
+       MAIN-RTN.
+           DISPLAY "HELLO WORLD!".
+       MAIN-EXT.
+           STOP RUN.
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# oc-dockerfile
+# opensource COBOL for Docker
+
 opensource COBOL and Open COBOL ESQL development environment
+
+Versions :
+- CentOS: centos7
+- opensource COBOL: 1.5.2J
+- Open COBOL ESQL: 1.2
+
+Sample :
+
+sample cobol run on docker image.
+
+```
+mkdir src
+wget https://github.com/n-isaka/OCDocker/blob/master/HELLO.cbl
+mv HELLO.cbl src/
+docker pull norisaka/opensource_cobol
+PWD=`pwd`
+docker run --rm -it -v ${PWD}/src:/oscobol/src:ro --name oscobol norisaka/opensource_cobol
+```
+
+in docker container
+
+```
+cobc src/HELLO.cbl
+cobcrun HELLO
+```
+
+display your console `HELLO WORLD!`
+
+When you want stop container, follow command:
+
+```
+docker stop oscobol
+```
+
+Copyright 2019, Tokyo System House Co., Ltd. <opencobol@tsh-world.co.jp>
